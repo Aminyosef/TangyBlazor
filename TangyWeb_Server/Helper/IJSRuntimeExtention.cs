474f8ecee120c7f2f965e4b82522daa22080ae5b
Amin Gomaa <amingomaa@hotmail.com>
2024-05-22 22:53:28 +0300

    22-5-2024

---
+++ TangyWeb_Server/Helper/IJSRuntimeExtention.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper
+{
+    public static class IJSRuntimeExtention
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jSRuntime,String message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToastrError(this IJSRuntime jSRuntime, String message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+
+        }
+    }
+}
