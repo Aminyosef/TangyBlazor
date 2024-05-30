c3065d43d23b0cd42d4beeb83c6816b80d50f492
Amin Gomaa <amingomaa@hotmail.com>
2024-05-30 17:57:54 +0300

    5/30/2024

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Tangy_DataAccess.Data;
 using TangyWeb_Server.Data;
+using Tangy_Business.Mapper;
+using Tangy_DataAccess;
+using Tangy_Business.Repository.IRepository;
+using Tangy_Business.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options
+.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
